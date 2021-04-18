58ef6edc667211f8740a756127448342cc64cff2
GABRIEL GÓMEZ <g.gomez12@alumnos.santotomas.cl>
2021-04-18 16:52:13 -0400

    Agregar archivos de proyecto.

---
+++ BancoST/Simulador_Credito.Designer.cs
@@ -0,0 +1,256 @@
+﻿
+namespace BancoST
+{
+    partial class Simulador_Credito
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCuotas = new System.Windows.Forms.TextBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.txtMontoTotal = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtValorCuotas = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Algerian", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(125, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(331, 71);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BANCO ST";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.txtNombre.Location = new System.Drawing.Point(46, 155);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(197, 29);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Location = new System.Drawing.Point(99, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label3.Location = new System.Drawing.Point(97, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Apellido";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApellido.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.txtApellido.Location = new System.Drawing.Point(46, 225);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(197, 29);
+            this.txtApellido.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label4.Location = new System.Drawing.Point(67, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Monto a solicitar";
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMonto.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.txtMonto.Location = new System.Drawing.Point(46, 301);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(197, 29);
+            this.txtMonto.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label5.Location = new System.Drawing.Point(67, 353);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(162, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Cantidad Cuotas";
+            // 
+            // txtCuotas
+            // 
+            this.txtCuotas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCuotas.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.txtCuotas.Location = new System.Drawing.Point(46, 380);
+            this.txtCuotas.Name = "txtCuotas";
+            this.txtCuotas.Size = new System.Drawing.Size(197, 29);
+            this.txtCuotas.TabIndex = 7;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.btnIniciar.Location = new System.Drawing.Point(320, 374);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(82, 35);
+            this.btnIniciar.TabIndex = 9;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // txtMontoTotal
+            // 
+            this.txtMontoTotal.Enabled = false;
+            this.txtMontoTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMontoTotal.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.txtMontoTotal.Location = new System.Drawing.Point(320, 225);
+            this.txtMontoTotal.Name = "txtMontoTotal";
+            this.txtMontoTotal.Size = new System.Drawing.Size(197, 29);
+            this.txtMontoTotal.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label6.Location = new System.Drawing.Point(352, 198);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(120, 24);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Monto Total";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label7.Location = new System.Drawing.Point(341, 274);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(163, 24);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Valor por cuotas";
+            // 
+            // txtValorCuotas
+            // 
+            this.txtValorCuotas.Enabled = false;
+            this.txtValorCuotas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValorCuotas.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.txtValorCuotas.Location = new System.Drawing.Point(320, 301);
+            this.txtValorCuotas.Name = "txtValorCuotas";
+            this.txtValorCuotas.Size = new System.Drawing.Size(197, 29);
+            this.txtValorCuotas.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label8.Location = new System.Drawing.Point(314, 150);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(164, 33);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Resultado:";
+            // 
+            // Simulador_Credito
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(566, 489);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtValorCuotas);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMontoTotal);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCuotas);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Simulador_Credito";
+            this.Text = "Simulador de Crédito";
+            this.Load += new System.EventHandler(this.Simulador_Credito_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMonto;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCuotas;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.TextBox txtMontoTotal;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtValorCuotas;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ BancoST/Simulador_Credito.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BancoST
+{
+    public partial class Simulador_Credito : Form
+    {
+        public Simulador_Credito()
+        {
+            InitializeComponent();
+        }
+
+        private void Simulador_Credito_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double interes, total, valor_cuotas;
+                double monto = double.Parse(txtMonto.Text);
+                int cuotas = int.Parse(txtCuotas.Text);
+
+                if (monto < 0)
+                {
+                    MessageBox.Show("El monto ingresado no puede ser negativo.", "Alerta", MessageBoxButtons.OK);
+                }
+                else if (monto < 500000)
+                {
+                    MessageBox.Show("Debe ingresar un minimo de $500.000 o superior en el monto.", "Alerta", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    if (cuotas < 1 || cuotas > 36)
+                    {
+                        MessageBox.Show("La cantidad de cuotas deben estar entre 1 y 36", "Alerta", MessageBoxButtons.OK);
+                    }
+                    else
+                    {
+                        if (cuotas >= 1 && cuotas <= 12)
+                        {
+                            interes = 0.1;
+                            // Redondeo para que muestre solo 2 decimales
+                            total = (double)decimal.Round((decimal)(monto + (monto * interes)), 2);
+                            valor_cuotas = (double)decimal.Round((decimal)(total / cuotas), 2);
+
+                            txtMontoTotal.Text = total.ToString();
+                            txtValorCuotas.Text = valor_cuotas.ToString();
+                        }
+                        else if (cuotas >= 13 && cuotas <= 24)
+                        {
+                            interes = 0.20;
+                            total = (double)decimal.Round((decimal)(monto + (monto * interes)), 2);
+                            valor_cuotas = (double)decimal.Round((decimal)(total / cuotas), 2);
+
+                            txtMontoTotal.Text = total.ToString();
+                            txtValorCuotas.Text = valor_cuotas.ToString();
+                        }
+                        else if (cuotas >= 25 && cuotas <= 36)
+                        {
+                            interes = 0.35;
+                            total = (double)decimal.Round((decimal)(monto + (monto * interes)), 2);
+                            valor_cuotas = (double)decimal.Round((decimal)(total / cuotas), 2);
+
+                            txtMontoTotal.Text = total.ToString();
+                            txtValorCuotas.Text = valor_cuotas.ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Verifique que haya ingresado los datos correctos en los campos.", "Alerta", MessageBoxButtons.OK);
+            }
+            
+
+        }
+    }
+}
